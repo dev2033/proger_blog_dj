1b2b040b5d6eb64b2a45332948db8a8e62dde9b2
dev2033 <dev.elop.02@mail.ru>
2021-03-15 16:02:03 +0300

    update

--- proger_blog/blog/urls.py
+++ proger_blog/blog/urls.py
@@ -5,6 +5,7 @@ from .views import *
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
-    path('projects/', AllProjects.as_view(), name='projects'),
+    path('projects/', GetProjectsListView.as_view(), name='projects'),
     path('project/<int:pk>/', ProjectDetailView.as_view(), name='view_project'),
+    path('posts/', GetPostsListView.as_view(), name='posts'),
 ]
--- proger_blog/blog/views.py
+++ proger_blog/blog/views.py
@@ -1,16 +1,14 @@
+from django.db.models import F
 from django.views.generic import ListView, DetailView
 
-from .models import Project, Category
+from .models import Project, Category, Post
 
 
 class Home(ListView):
-    """
-    Вывод главной страницы
-    """
+    """Вывод главной страницы"""
     model = Project
     context_object_name = 'projects'
     template_name = 'blog/index.html'
-    allow_empty = True
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
@@ -31,17 +29,48 @@ class ProjectDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Proger Blog'
+        projects = Project.objects.all()
+        # показывает 3 последних проекта
+        context['last_projects'] = projects.order_by('-id')[:3]
+        context['categories'] = Category.objects.all()
         return context
 
 
-class AllProjects(ListView):
+class GetProjectsListView(ListView):
     """Вывод всех проектов"""
     model = Project
     context_object_name = 'projects'
     template_name = 'blog/projects.html'
-    allow_empty = True
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
         context['title'] = 'Proger Blog : Проекты'
+        return context
+
+
+class GetPostsListView(ListView):
+    """Выводит все посты для блога"""
+    model = Post
+    context_object_name = 'posts'
+    template_name = 'blog/blog.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Proger Blog : Посты'
+        return context
+
+
+class GetPost(DetailView):
+    """Показывает каждый пост отдельно"""
+    model = Post
+    template_name = 'blog/blog_post.html'
+    context_object_name = 'post'
+    allow_empty = True
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # выражение, которое увеличивает кол-во просмотров
+        self.object.views = F('views') + 1
+        self.object.save()
+        self.object.refresh_from_db()
         return context
